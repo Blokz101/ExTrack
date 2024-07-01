8a214193c99e371ff4f5c688e30250521740bdc9
Blokz101 <mquantwenty22@gmail.com>
2024-07-01 18:08:09 -0400

    [Testing] Added close after edits test.

--- tests/test_view/test_TransactionPopup.py
+++ tests/test_view/test_TransactionPopup.py
@@ -278,6 +278,59 @@ class TestTransactionPopup(Sample1TestCase):
 
         popup.window.close()
 
+    def test_close_after_edits(self):
+        """
+        Tests making edits to the transaction popup and then closing it.
+        """
+        expected_transactions: list[Transaction] = Transaction.get_all()
+        expected_amounts: list[Amount] = Amount.get_all()
+
+        popup: TransactionPopup = TransactionPopup(3)
+        _, _ = popup.window.read(timeout=0)
+
+        # Fake user inputs
+        key: str = "-ACCOUNT SELECTOR-"
+        new_account_id: int = 2
+        popup.window[key].update(value=Account.from_id(new_account_id))
+        popup.check_event(key, {key: Account.from_id(new_account_id)})
+
+        key = "-DESCRIPTION INPUT-"
+        new_description: str = "This is an edited transaction"
+        popup.window[key].update(value=new_description)
+        popup.check_event(key, {key: new_description})
+
+        key = "-MERCHANT SELECTOR-"
+        new_merchant_id: int = 1
+        popup.window[key].update(value=Merchant.from_id(new_merchant_id))
+        popup.check_event(key, {key: Merchant.from_id(new_merchant_id)})
+
+        key = "-COORDINATE INPUT-"
+        new_lat: float = 35.86351608517815
+        new_long: float = -78.64574508597906
+        popup.window[key].update(value=f"{new_lat}, {new_long}")
+        popup.check_event(key, {key: f"{new_lat}, {new_long}"})
+
+        key = "-DATE INPUT-"
+        new_date: str = "07/24/2004 12:45:31"
+        popup.window[key].update(value=new_date)
+        popup.check_event(key, {key: new_date})
+
+        # Edit amounts
+        popup.window[("-AMOUNT ROW DESCRIPTION-", 0)].update(value="Graphics Card")
+        popup.check_event(("-AMOUNT ROW DESCRIPTION-", 0), {})
+        popup.window["-TOTAL AMOUNT INPUT-"].update(value="803.54")
+        popup.check_event("-TOTAL AMOUNT INPUT-", {})
+        popup.window[("-AMOUNT ROW AMOUNT-", 0)].update(value="803.54")
+        popup.check_event(("-AMOUNT ROW AMOUNT", 0), {})
+        popup.amount_rows[0].tag_list = [Tag.from_id(10), Tag.from_id(5)]
+        popup.check_event(None, {})
+
+        # Exit the popup
+        popup.check_event("Exit", {})
+
+        self.assertSqlListEqual(expected_transactions, Transaction.get_all())
+        self.assertSqlListEqual(expected_amounts, Amount.get_all())
+
     def test_submit_basic_input_edits(self):
         """
         Tests editing the database with the basic input fields.
